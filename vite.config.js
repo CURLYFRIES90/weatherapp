8e9780418bc57b61301d15cacda19380d2f57ef6
Tracey <amourtracey1990@gmail.com>
2025-12-08 22:45:39 -0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-<<<<<<< HEAD
-=======
-
->>>>>>> eee8f678e82c5d101ac91393a2bcadfa6127d891
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  base: '/weatherapp/', 
-=======
->>>>>>> eee8f678e82c5d101ac91393a2bcadfa6127d891
+  base: '/weatherapp/',  // Make sure this matches your repo name
   plugins: [
     vue(),
     vueDevTools(),
